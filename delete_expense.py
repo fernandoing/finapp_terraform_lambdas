ca177793d891908f1f59cc760995d5b153555a3e
David Hernandez Morones <david_hernandez1@epam.com>
2024-03-20 10:04:02 -0600

    Added authorization to delete expense

--- delete_expense.py
+++ delete_expense.py
@@ -1,6 +1,7 @@
 from expenses_persistence import ExpenseRepositoryImplementation as Repository
 from pymysql import MySQLError
 
+import jwt
 import os
 
 
@@ -10,21 +11,41 @@ def handler(event, context):
     password = os.environ['RDS_PASSWORD']
     db_name = os.environ['RDS_DB_NAME']
     db_port = os.environ['RDS_PORT']
+    secret_key = os.environ['SECRET_KEY']
 
     id = event['pathParameters']['id']
+    token = event['Authorization'].split(' ')[1]
+    token_data = jwt.decode(jwt=token, key=secret_key, algorithms=['HS256'])
+    user_id = token_data.get('user_id')
 
     try:
-        deleted = Repository(
+        repo = Repository(
             host=rds_host,
             db_port=int(db_port),
             user=name,
             password=password,
             db_name=db_name
-        ).delete(int(id))
+        )
+        expense_by_user = repo.get_by(user_id=user_id, expense_id=id)
+
+        if not expense_by_user:
+            return {
+                'deleted': False
+            }
+
+        if len(expense_by_user) > 1:
+            return {
+                'deleted': False
+            }
+        deleted = repo.delete(int(id))
+
     except MySQLError:
         return {
             'error': 'Internal server error'
         }
+
+    finally:
+        repo.__del__()
 
     return {
         'deleted': deleted
