334da0482bf2d139178b586b78f18f35095973b0
David Hernandez Morones <david_hernandez1@epam.com>
2024-03-20 10:54:23 -0600

    Added finally to try block

--- get_expenses.py
+++ get_expenses.py
@@ -15,23 +15,22 @@ def handler(event, context):
     secret_key = os.environ['SECRET_KEY']
 
     token = event['Authorization'].split(' ')[1]
-
     token_data = jwt.decode(token, secret_key, algorithms=["HS256"])
-
     user_id = token_data.get('user_id')
 
     try:
-        records = Repository(
+        repo = Repository(
             host=rds_host,
             user=name,
             password=password,
             db_port=int(db_port),
             db_name=db_name
-        ).get_by(user_id=user_id)
+        )
+        records = repo.get_by(user_id=user_id)
     except MySQLError:
-        return {
-            'error': 'Internal server error'
-        }
+        raise Exception('Internal server error')
+    finally:
+        repo.__del__()
 
     if records is None:
         return {
