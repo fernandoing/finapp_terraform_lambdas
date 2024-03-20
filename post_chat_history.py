e63212447a733e32eba2e9d0539deb613fc1d102
David Hernandez Morones <david_hernandez1@epam.com>
2024-03-20 10:44:48 -0600

    Added Exception when MySQLError raises

--- post_chat_history.py
+++ post_chat_history.py
@@ -15,11 +15,8 @@ def handler(event, context):
     secret_key = os.environ['SECRET_KEY']
 
     token = event['Authorization'].split(' ')[1]
-
     token_data = jwt.decode(token, secret_key, algorithms=["HS256"])
-
     user_id = token_data.get('user_id')
-
     body: dict = event['chat_history']
 
     chat_history = ChatHistory(
@@ -28,17 +25,18 @@ def handler(event, context):
     )
 
     try:
-        chat_id = Repository(
+        repo = Repository(
             host=rds_host,
             db_port=int(db_port),
             user=name,
             password=password,
             db_name=db_name
-        ).add(chat_history)
+        )
+        chat_id = repo.add(chat_history)
     except MySQLError:
-        return {
-            'error': 'Internal server error'
-        }
+        raise Exception('Internal server error')
+    finally:
+        repo.__del__()
 
     return {
         'chat_id': chat_id
