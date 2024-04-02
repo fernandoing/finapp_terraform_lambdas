e1a8be83ef8acf4cf0b441e51aa5efbc9aff3ad4
David Hernandez Morones <david_hernandez1@epam.com>
2024-04-02 15:36:33 -0600

    Fixed database name

--- delete_chats.py
+++ delete_chats.py
@@ -10,7 +10,7 @@ import os
 def handler(event, context):
     secret_name = os.environ['SECRETS_NAME']
     secrets = get_secrets(secret_name)
-    db_name = 'chat_history'
+    db_name = 'chats'
     db_host = secrets.get('db_host')
     db_port = secrets.get('db_port')
     db_user = secrets.get('username')
